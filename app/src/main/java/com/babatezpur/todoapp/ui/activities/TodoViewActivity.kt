66f4b814ea6fdf5ef86c3650d1bec333e7fb64d4
babatezpur <babatezpur@gmail.com>
2025-06-16 10:35:57 +0530

    Add basic file structures

---
+++ app/src/main/java/com/babatezpur/todoapp/ui/activities/TodoViewActivity.kt
@@ -0,0 +1,12 @@
+package com.babatezpur.todoapp.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.babatezpur.todoapp.R
+
+class TodoViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo_view)
+    }
+}
